2cf09e6d0ca714479b89c18093a17afce3d18f40
kausar mia <mdkousarmia71@gmail.com>
2025-01-19 13:00:31 +0600

    admin profile page impliment

--- index.js
+++ index.js
@@ -29,12 +29,56 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        const tagesCullection = client.db("Postify").collection("tages");
+        const AnnouncementCullection = client.db("Postify").collection("Announcement");
         const bageCullection = client.db("Postify").collection("bage");
         const addpostCullection = client.db("Postify").collection("addpost");
         const comentsCullection = client.db("Postify").collection("coments");
         const feedbackCullection = client.db("Postify").collection("feedback");
+        const mackAdminCullection = client.db("Postify").collection("mackAdmin");
 
 
+        // tags added for admin profile page
+        app.post('/tags', async (req, res) => {
+            const data = req.body;
+            const result = await tagesCullection.insertOne(data);
+            res.send(result)
+        })
+        // tags data fatch
+        app.get('/tags', async (req, res) => {
+            const tags = req.body;
+            const result = await tagesCullection.find(tags).toArray()
+            res.send(result)
+        })
+
+        // Announcement added for admin Announcement page
+        app.post('/announcement', async (req, res) => {
+            const data = req.body;
+            const result = await AnnouncementCullection.insertOne(data);
+            res.send(result)
+        })
+        // Announcement fatch for admin Announcement page
+        app.get('/announcement', async (req, res) => {
+            const Announc = req.body;
+            const result = await AnnouncementCullection.find(Announc).toArray()
+            res.send(result)
+        })
+
+        // mack admin
+        app.post('/mackAdmin', async (req, res) => {
+            const data = req.body;
+            const result = await mackAdminCullection.insertOne(data);
+            res.send(result)
+        })
+
+        // mackadmin data fatch fatch 
+        app.get('/mackAdmin', async (req, res) => {
+            const admin = req.body;
+            const result = await mackAdminCullection.find(admin).toArray()
+            res.send(result)
+        })
+
+        // bage added
         app.post('/users/:email', async (req, res) => {
             const bage = req.body;
             const email = req.params.email;
@@ -49,12 +93,12 @@ async function run() {
             const result = await bageCullection.insertOne(bage);
             res.send(result)
         })
-
+        // bage gat
         app.get('/users', async (req, res) => {
             const result = await bageCullection.find().toArray()
             res.send(result)
         })
-
+        // bage update
         app.put('/users/:email', async (req, res) => {
             const email = req.params.email;
             console.log('bage', email);
@@ -72,8 +116,8 @@ async function run() {
             const commentId = req.params.id;
             const { feedback } = req.body;
             const feedbackData = {
-                commentId,  
-                feedback,  
+                commentId,
+                feedback,
                 createdAt: new Date(),
             };
             const result = await feedbackCullection.insertOne(feedbackData);
@@ -91,7 +135,7 @@ async function run() {
 
         app.get('/posts/popularity', async (req, res) => {
             const { search, sortByPopularity } = req.query;
-            const { carentTime } = req.params;
+            // const {carentTime} = req.body;
             // console.log('currentitem=', carentTime);
             const searchFilter = search ? { tag: { $regex: search, $options: 'i' } } : {};
 
@@ -108,7 +152,7 @@ async function run() {
                     { $sort: { voteDifference: -1 } },
                 ]).toArray();
             } else {
-                result = await addpostCullection.find(searchFilter).sort({ carentTime: 1 }).toArray();
+                result = (await addpostCullection.find(searchFilter).sort(carentTime - 1).toArray())
             }
             res.send(result);
         });
@@ -122,6 +166,10 @@ async function run() {
             const result = await addpostCullection.find(searchFilter).sort({ carentTime: -1 }).toArray()
             res.send(result)
         })
+
+
+
+
 
         // DELETE API Endpoint
         app.delete('/delete/:id', async (req, res) => {
